f41fc814b68075324316533a6beb8eaee5acf2e5
ilia1 <ilia1@DESKTOP-0ETRE1V>
2021-10-08 16:15:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace labRob4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+        m1: Console.WriteLine("Введите для точки М" + " координату по Х = ");
+            float x = float.Parse((Console.ReadLine()));
+            Console.WriteLine("Введите для точки М" + " координату по Y = ");
+            float y = float.Parse((Console.ReadLine()));
+
+            if (y < 0)
+            {
+                if (y + x * x < 2)
+                {
+                    if (x >= 0) N = 2;
+                    else N = 1;
+                }
+                else {
+                    if (x >= 0) N = 4;
+                    else N = 3;
+                }
+            }
+            else {
+                if (x >= 0) N = 3;
+                else N = 2;
+            
+            }
+            Console.WriteLine('\t' + "      РЕЗУЛЬТАТ:");
+            Console.WriteLine("    Точка М(" + x + ";" + y + ")" + " Лежит в области с N = " + N);
+            Console.WriteLine('\n' + "Для повторного ввода" + "набрать любую клавишу." + '\n' + "Для завершения програмы нажмите Enter.");
+            string p = Console.ReadLine();
+            if (p != "") goto m1;
+
+        }
+    }
+}
